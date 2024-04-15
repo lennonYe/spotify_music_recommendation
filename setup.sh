217008799c5d3ec141d4b2e57294582315ecac40
lennonYe <lennonye123@gmail.com>
2024-04-15 18:34:50 -0400

    deleted proc and setup for streamlit

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# 示例：设置环境变量
-echo "Setting up environment variables"
-export SPOTIPY_CLIENT_ID="5b690855f0664257a567c12a16db3b19"
-export SPOTIPY_CLIENT_SECRET="f820a8c138654e70850d1ac7bc4f4c8e"
-export SPOTIPY_REDIRECT_URI="https://spotify-recommendation-sys.streamlit.app/"
