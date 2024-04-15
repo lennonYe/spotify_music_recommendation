6ca808402986d6d5e60cd0358015b601cab80bbc
lennonYe <lennonye123@gmail.com>
2024-04-15 18:18:20 -0400

    app revised

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 echo "Setting up environment variables"
 export SPOTIPY_CLIENT_ID="5b690855f0664257a567c12a16db3b19"
 export SPOTIPY_CLIENT_SECRET="f820a8c138654e70850d1ac7bc4f4c8e"
-export SPOTIPY_REDIRECT_URI="https://spotify-recommendation-syst.streamlit.app"
+export SPOTIPY_REDIRECT_URI="https://spotify-recommendation-sys.streamlit.app"
